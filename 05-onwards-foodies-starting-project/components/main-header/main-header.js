848a4974196c420805ade423191c95a8148cd199
hung1503 <hungnguyen150300@gmail.com>
2025-05-12 17:00:46 +0300

    Adding components

---
+++ 05-onwards-foodies-starting-project/components/main-header/main-header.js
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import logoImg from "@/assets/logo.png";
+
+import styles from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link className={styles.logo} href="/">
+          <Image src={logoImg} alt="Logo" priority />
+          NextLevel Food
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}

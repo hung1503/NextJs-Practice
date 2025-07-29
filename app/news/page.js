d17a6f410df220e1bbe07ce8e946a89de4081c8c
hung1503 <hungnguyen150300@gmail.com>
2025-07-29 17:13:50 +0300

    Adding paralel route and catch-all route

--- app/news/page.js
+++ app/news/page.js
@@ -1,21 +1,11 @@
-import Link from "next/link";
-
+import NewsList from "@/components/news-list";
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function NewsPage() {
   return (
     <>
       <h1>News Page</h1>
-      <ul className="news-list">
-        {DUMMY_NEWS.map((news) => (
-          <li key={news.id}>
-            <Link href={`/news/${news.slug}`}>
-              <img src={`/images/news/${news.image}`} alt={news.title} />
-              <span>{news.title}</span>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <NewsList news={DUMMY_NEWS} />
     </>
   );
 }

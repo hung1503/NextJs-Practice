18c872a355db5a71a56171ddc4a5994a9053cf5a
hung1503 <hungnguyen150300@gmail.com>
2025-07-29 15:38:30 +0300

    Adding not-found error handling

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function ArticleNotFoundPage() {
+  return (
+    <div id="error">
+      <h1>The article Not Found</h1>
+      <p>The article you are looking for does not exist.</p>
+    </div>
+  );
+}

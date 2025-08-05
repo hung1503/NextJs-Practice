f43be4374138c41cc4abb992c36d5be9c0eff67a
hung1503 <hungnguyen150300@gmail.com>
2025-08-06 00:29:56 +0300

    Adding route groups

--- 02-routing-rendering/app/(content)/news/[slug]/@modal/(.)image/page.js
+++ 02-routing-rendering/app/(content)/news/[slug]/@modal/(.)image/page.js
@@ -1,17 +1,24 @@
+"use client";
+
+import { notFound, useRouter } from "next/navigation";
+
 import { DUMMY_NEWS } from "@/dummy-news";
-import { notFound } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
+
   const newsItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsItemSlug
   );
+
   if (!newsItem) {
     notFound();
   }
+
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
